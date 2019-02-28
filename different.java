3ee641c8254d2106736e89abadef7c0367ad9e17
sarahTheSlayer2 <sarquinn123@gmail.com>
2019-02-28 15:26:48 -0600

    Take this

---
+++ different.java
@@ -0,0 +1,7 @@
+public class different {
+
+	public static void main(String[] args) {
+		System.out.println("Hey there");
+	}
+
+}
--- runthis.java
+++ runthis.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 public class runthis {
 
 public static void main(String[] args) {
-	System.out.println("If this is Justin, you are gay");
-
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter your name: ");
+	String the = sc.nextLine();
+	switch(the) {
+		case "Declan" : 
+		case "Justin" :
+		case "Ryan" :
+			System.out.println("You are stupid");
+			break;
+		case "Sarah" :
+			System.out.println("You arrr awesome");
+			break;
+		default :
+			System.out.println("Get better at typing your name loser.");
+	}
 
 	}
 }
