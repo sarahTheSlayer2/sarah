49082ac3c9079193888ed1a0b60d158d3cfbb78e
djgundrum <declan.gundrum.17@gmail.com>
2019-02-28 09:33:59 -0600

    More classes

---
+++ sunflower.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sunflower {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter name:");
+		String name = scnr.nextLine();
+		if (name.equals("Declan")) {
+			System.out.println("u suk");
+		}
+		else {
+		System.out.println("u r ok");
+		}
+	}
+
+}
